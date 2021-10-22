c33ccfcc53a237436b7c148346c022381791cd40
cr-jkb <81223644+cr-jkb@users.noreply.github.com>
2021-10-22 12:37:45 +0200

    the full test file for main has been written
    testet induktiv alle Fälle

--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -1,9 +1,21 @@
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
-
+val eol = sys.props("line.separator")
 class GridSpec extends AnyWordSpec:
   "4-Gewinnt" should {
-    "have a grid as String of form 7x6" in {
-      SpielBrett(1,2,3) should be("+---+---+---+---+---+---+---+")
+    "have a scalable separation bar" in {
+      spielbrett_separator(1, 1) should be { "+-+" + eol }
+      spielbrett_separator(2, 1) should be { "+-+-+" + eol }
+      spielbrett_separator(1, 2) should be { "+--+" + eol }
+    }
+
+    "have scalable slot cells" in {
+      spielbrett_slots(1, 1) should be { "| |" + eol }
+      spielbrett_slots(2, 1) should be { "| | |" + eol }
+      spielbrett_slots(1, 2) should be { "|  |" + eol }
+    }
+
+    "have a valid and scalable game field" in {
+      SpielBrett(1, 1, 1) should be { "+-+" + eol + "| |" + eol + "+-+" + eol }
     }
   }
