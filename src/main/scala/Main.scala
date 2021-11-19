1fabfafeea3b5bca51c86fb03136fa419bcd9bc9
cr-jkb <81223644+cr-jkb@users.noreply.github.com>
2021-11-19 12:51:45 +0100

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,11 @@
 import aview.TUI
 import controller.Controller
 import model.Field
-import model.Matrix
 import model.Stone
-
 @main
 def hello: Unit = {
 	val eol = sys.props("line.separator")
-	println(eol + "Hochschule fuer Technik, Wirtschaft & Gestaltung")
-	println("AIN SOFTWARE-ENGINEERING WiSe 21/22")
-	println("        ### GRUPPE 15 ###")
-	println(eol + ">  Willkommen zu 4-Gewinnt  <")
+	println(eol + "Hochschule fuer Technik, Wirtschaft & Gestaltung" + eol + "AIN SOFTWARE-ENGINEERING WiSe 21/22" + eol + "        ### GRUPPE 15 ###" + eol + eol + ">  Willkommen zu 4-Gewinnt  <")
 	val field = new Field(6, 7, Stone.Empty)
 	val controller = Controller(field)
 	val tui = TUI(controller)
