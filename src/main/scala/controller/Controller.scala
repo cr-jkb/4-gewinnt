20153b076845bbfe43e41ea281934e40b3a6d48d
JulianMennel <92576873+JulianMennel@users.noreply.github.com>
2021-11-19 13:13:37 +0100

    Added a new Constructor

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -5,6 +5,7 @@ import model.Stone
 import util.Observable
 
 case class Controller(var field: Field) extends Observable:
+  def this() = this(new Field())
   def put(stone: Stone, x: Int, y: Int) =
     field = field.put(stone, x, y)
     notifyObservers
