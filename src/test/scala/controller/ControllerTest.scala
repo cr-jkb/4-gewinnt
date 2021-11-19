21284619b56b36ddf6b455476faf90cf01bb54c1
JulianMennel <92576873+JulianMennel@users.noreply.github.com>
2021-11-19 13:10:04 +0100

    Implemented ControllerTest

---
+++ src/test/scala/controller/ControllerTest.scala
@@ -0,0 +1,33 @@
+package controller
+
+import util.Observer
+import model.Stone
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers.*
+
+class ControllerSpec extends AnyWordSpec {
+  "A controller" when {
+    "observed by an Observer" should {
+      val controller = new Controller()
+      val observer = new Observer {
+        var updated: Boolean = false
+
+        override def update: Unit = updated = !updated
+
+        override def toString: String = updated.toString
+      }
+
+      "notify when insert a chip" in {
+        controller.add(observer)
+        controller.put(Stone.Empty, 1, 1)
+        observer.toString should be("true")
+      }
+
+      "after remove should not notify" in {
+        controller.remove(observer)
+        controller.put(Stone.Empty, 1, 1)
+        observer.toString should be("true")
+      }
+    }
+  }
+}
