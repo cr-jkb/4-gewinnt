70830cb3e1686270c395e97ee69a28034ea3a82d
JulianMennel <menneljulian@gmail.com>
2021-11-26 13:38:15 +0100

    added test cases for ComputerModeStrategy

--- src/main/scala/model/ComputerModeStrategy.scala
+++ src/main/scala/model/ComputerModeStrategy.scala
@@ -6,14 +6,9 @@ import util.PlayerState
 case class ComputerModeStrategy() extends ModeStrategy {
   override def put(x: Int, y: Int, field: Field): Field = {
     var field2 = field.player.put(x, y, field)
-    if (y + 1 <= field.size2)
+    if (y + 1 <= field.size2 - 1)
       field2.player.put(x, y + 1, field2)
-    else if (y - 1 >= 1)
-      field2.player.put(x, y - 1, field2)
-    else if (x - 1 >= 1)
-      field2.player.put(x - 1, y, field2)
     else
-      println("ERROR!!")
       field2
   }
 }
---
+++ src/test/scala/model/ComputerModeStrategyTest.scala
@@ -0,0 +1,25 @@
+package model
+
+import controller.Controller
+import org.scalatest.matchers.should.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+
+class ComputerModeStrategySpec extends AnyWordSpec {
+  "ComputerModeStrategy" should {
+    var field1 = new Field(1, 2, Stone.Empty)
+    var field2 = new Field(1, 2, Stone.Empty)
+    var field3 = new Field(1, 2, Stone.Empty)
+    var field4 = new Field(1, 2, Stone.Empty)
+    field2 = field2.put(0, 0)
+    field2 = field2.put(0, 1)
+    field4 = field4.put(0, 1)
+
+    "have a valid computer input" in {
+
+      ComputerModeStrategy().put(0, 0, field1) should be(field2)
+    }
+    "have a invalid computer input" in {
+      ComputerModeStrategy().put(0, 1, field3) should be(field4)
+    }
+  }
+}
