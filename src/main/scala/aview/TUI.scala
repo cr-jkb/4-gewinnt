f2bccf3cd8174008d3ebab0a61cf6826ef6ea15b
JulianMennel <menneljulian@gmail.com>
2021-12-03 11:43:14 +0100

    Update TUI.scala

--- src/main/scala/aview/TUI.scala
+++ src/main/scala/aview/TUI.scala
@@ -5,6 +5,7 @@ import model.Stone
 import model.Move
 import scala.io.StdIn.readLine
 import util.Observer
+import scala.util.{Try,Success,Failure}
 
 class TUI(controller: Controller) extends Observer:
   controller.add(this)
@@ -26,6 +27,7 @@ class TUI(controller: Controller) extends Observer:
           case 'p' => controller.setMode("player"); println("Modus wurde gewechselt.")
           case 'r' => controller.redo; println("Redo erfolgreich.")
           case 'u' => controller.undo; println("Undo erfolgreich.")
+          case 'e' => println("Bitte Eingabe ueberpruefen.")
         inputLoop()
 
   def analyseInput(input: String): Option[Move] =
@@ -36,11 +38,24 @@ class TUI(controller: Controller) extends Observer:
       case "computer" => Some(Move('c', 0, 0))
       case "player" => Some(Move('p', 0, 0))
       case _ => {
-        val chars = input.toCharArray
-        chars(0) match
-          case 'i' =>
-            val x = chars(2).toString.toInt
-            val y = chars(4).toString.toInt
-            Some(Move('i', x - 1, y - 1))
-          case _ => None
+        getCharArray(input) match
+          case Success(chars) =>
+            chars(0) match {
+              case 'i' =>
+                getInt(chars(2)) match
+                  case Success(x) =>
+                    getInt(chars(4)) match
+                      case Success(y) => Some(Move('i', x - 1, y - 1))
+                      case Failure(y) => Some(Move('e', 0, 0))
+                  case Failure(x) => Some(Move('e', 0, 0))
+              case _ => Some(Move('e', 0, 0))
+            }
+          case Failure(err) => println("Eingabe entspricht nicht der vorgegebenen Laenge."); Some(Move('e', 0, 0))
       }
+
+  def getInt(input: Char): Try[Int] = Try(input.toString.toInt)
+  def getCharArray(input: String): Try[Array[Char]] = Try {
+    if (input.length != 5)
+      return Failure(new ArrayIndexOutOfBoundsException)
+    input.toCharArray
+  }
