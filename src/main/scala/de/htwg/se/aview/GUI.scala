0519ef393d509edebf447c7db940d531a80bea0f
chris-jakob <81223644+cr-jkb@users.noreply.github.com>
2022-01-25 01:10:47 +0100

    Update GUI.scala

--- src/main/scala/de/htwg/se/aview/GUI.scala
+++ src/main/scala/de/htwg/se/aview/GUI.scala
@@ -4,37 +4,29 @@ import de.htwg.se.controller.controllerComponent.ControllerInterface
 import scala.swing._
 import de.htwg.se.util.Observer
 import scala.swing.event._
+import javax.swing.{BorderFactory, ImageIcon}
 import scala.swing.Swing.LineBorder
 import javax.imageio.ImageIO
 import java.io.File
 
+//GridPanels new GridPanel(rows, cols)
+
 class GUI(controller: ControllerInterface) extends Observer:
   controller.add(this)
-
+  //border = BorderFactory.createLineBorder(Color.BLACK, 5)
   var button = Array.ofDim[Button](controller.field.size, controller.field.size2)
   override def update: Unit = redraw()
   override def kill: Unit = System.exit(0)
 
-  val icon = ImageIO.read(new File("res/logo.jpg"))
+  val MainIcon = ImageIO.read(new File("res/logo.jpg"))
   
   val frame = new Frame {
     title = "4-Gewinnt - Gruppe 15"
     override def closeOperation(): Unit = controller.quit
-    iconImage = icon
+    iconImage = MainIcon
 
-    val spielfeld = new GridPanel(controller.field.size, controller.field.size2) {
-      border = LineBorder(java.awt.Color.GRAY, 2)
-      background = java.awt.Color.BLACK
-      for (index <- 0 until controller.field.size;
-           index2 <- 0 until controller.field.size2)
-          button(index)(index2) = new Button(controller.field.get(index, index2).toString) {
-            reactions += { case event.ButtonClicked(_) =>
-              controller.put(index, index2)
-              button(index)(index2).text = controller.field.get(index, index2).toString
-            }
-          }
-          contents += button(index)(index2)
-    }
+    val spielfeld = getField(controller.field.size, controller.field.size2)
+    val selector = getSelector(controller.field.size)
 
     val optionen = new GridPanel(2, 2) {
       border = LineBorder(java.awt.Color.WHITE, 20)
@@ -102,6 +94,7 @@ class GUI(controller: ControllerInterface) extends Observer:
 
     val frameContents = new BoxPanel(Orientation.Vertical) {
       contents += myMenu
+      contents += selector
       contents += spielfeld
       contents += optionen
     }
@@ -117,7 +110,43 @@ class GUI(controller: ControllerInterface) extends Observer:
   def redraw(): Unit =
     for (index <- 0 to controller.field.size - 1)
       for (index2 <- 0 to controller.field.size2 - 1)
-        button(index)(index2).text = controller.field.get(index, index2).toString
+        //button(index)(index2).icon = controller.field.get(index, index2).toString
+        if (controller.field.get(index, index2).toString.equals("X")) {
+          button(index)(index2).icon = new ImageIcon(ImageIO.read(new File("res/slotyellow.png")))
+        } else if (controller.field.get(index, index2).toString.equals("O")) {
+          button(index)(index2).icon = new ImageIcon(ImageIO.read(new File("res/slotred.png")))                    
+        }
 
   //def closeOperation(): Unit =
     //controller.quit
+
+  def getField(size: Int, size2: Int): GridPanel = 
+    new GridPanel(size, size2) {
+        //border = LineBorder(java.awt.Color.GRAY, 2)
+        background = java.awt.Color.BLACK
+        for (index <- 0 until controller.field.size;
+            index2 <- 0 until controller.field.size2)
+            button(index)(index2) = new Button(controller.field.get(index, index2).toString) {
+              background = java.awt.Color.white
+              icon = new ImageIcon(ImageIO.read(new File("res/slot.png")))
+              reactions += { case event.ButtonClicked(_) =>
+                controller.put(index, index2)
+                //button(index)(index2).text = controller.field.get(index, index2).toString
+                /*if (controller.field.get(index, index2).toString.equals("X")) {
+                  icon = new ImageIcon(ImageIO.read(new File("res/slotyellow.png")))
+                } else if (controller.field.get(index, index2).toString.equals("O")) {
+                  icon = new ImageIcon(ImageIO.read(new File("res/slotred.png")))                    
+                }*/
+              }
+            }
+            contents += button(index)(index2)
+      }
+  
+  def getSelector(sizeW: Int): GridPanel =
+    new GridPanel(0, sizeW) {
+      for (i <- 0 until sizeW)
+        val select = new Label {
+          icon = new ImageIcon(ImageIO.read(new File("res/red2-lq.png")))
+        }
+        contents += select
+    }
