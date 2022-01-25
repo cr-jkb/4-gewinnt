d45aee18fffb0dde2e03763ec9dcb7979b39e1a1
chris-jakob <81223644+cr-jkb@users.noreply.github.com>
2022-01-25 18:57:59 +0100

    Update TruePlayerState.scala

--- src/main/scala/de/htwg/se/model/fieldComponent/fieldBaseImpl/TruePlayerState.scala
+++ src/main/scala/de/htwg/se/model/fieldComponent/fieldBaseImpl/TruePlayerState.scala
@@ -3,10 +3,19 @@
 package de.htwg.se.model.fieldComponent.fieldBaseImpl
 
 import de.htwg.se.util.PlayerState
+import scala.util.control.Breaks._
+
+object SpaceFound extends Exception
 
 case class TruePlayerState() extends PlayerState {
   override def put(x: Int, y: Int, field: Field): Field = { // to be called from field.player in Field.scala and ___ModeStrategy.scala{2}
     field.player = FalsePlayerState() // hand ball over to the other player (to 'O')
-    field.copy(field.matrix.replaceCell(x, y, Stone.X))
+    var low_x = 0
+    breakable {
+      for (low_x <- (field.matrix.size-1 until 0)) { //gehe von unten los
+        if (field.matrix.cell(low_x, y).toString.equals(" ")) break;
+      }
+     }
+    field.copy(field.matrix.replaceCell(low_x, y, Stone.X))
   }
 }
