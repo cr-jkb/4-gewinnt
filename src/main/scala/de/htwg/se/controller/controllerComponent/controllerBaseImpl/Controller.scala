04a14a3a3cf59d0e4cd7ba7a2c1ba8bad47271ab
yasmoonx <yasminhoffmann99@web.de>
2023-04-04 17:12:52 +0200

    use Try Monad instead of try/catch

--- src/main/scala/de/htwg/se/controller/controllerComponent/controllerBaseImpl/Controller.scala
+++ src/main/scala/de/htwg/se/controller/controllerComponent/controllerBaseImpl/Controller.scala
@@ -16,35 +16,67 @@ import com.google.inject.Inject
 import com.google.inject.name.Named
 object SpaceFound extends Exception
 object FullRow extends Exception
+import scala.util.{Try, Success, Failure}
 
-case class Controller @Inject()(@Named("DefField") var field: FieldInterface) extends ControllerInterface with Observable:
+case class Controller @Inject() (@Named("DefField") var field: FieldInterface)
+    extends ControllerInterface
+    with Observable:
 
   val undoManager = new UndoManager[FieldInterface]
   val injector = Guice.createInjector(new MainModule)
   val fileIo = injector.getInstance(classOf[FileIOInterface])
   var error = ""
   var gameWon = false
-  
-  override def setGameState(won: Boolean) = {gameWon = won}
+
+  override def setGameState(won: Boolean) = { gameWon = won }
   def newField =
     gameWon = false
-    field = new Field() //no need for new if "instance-ized" with parameters
+    field = new Field() // no need for new if "instance-ized" with parameters
     notifyObservers
-  
+
+  // def put(x: Int, y: Int) =
+  //   var low_x = field.size - 1
+  //   try {
+  //     for (try_x <- field.size - 1 to 0 by -1) { // gehe von unten los
+  //       if (field.get(try_x, y) == Stone.Empty) {
+  //         low_x = try_x; throw SpaceFound;
+  //       }
+  //     }
+  //     throw FullRow
+  //   } catch {
+  //     case SpaceFound =>
+  //       undoManager.clearRedo();
+  //       field = undoManager.doStep(field, new PutCommand(low_x, y, this));
+  //       error = "Stone successfully set" // print(s"will put to ${low_x}\n")
+  //     case FullRow =>
+  //       error = (s"the vertical row at ${y + 1} is full\n") // throw noAction
+  //   }
+  //   notifyObservers
+
   def put(x: Int, y: Int) =
-    var low_x = field.size-1
-    try {      
-      for (try_x <- field.size-1 to 0 by -1) { //gehe von unten los
-        if (field.get(try_x, y) == Stone.Empty) { low_x = try_x; throw SpaceFound; }
+    var low_x = field.size - 1
+    val result = Try {
+      for (try_x <- field.size - 1 to 0 by -1) {
+        if (field.get(try_x, y) == Stone.Empty) {
+          low_x = try_x
+          throw SpaceFound
+        }
       }
       throw FullRow
-    } catch {
-    case SpaceFound => undoManager.clearRedo(); field = undoManager.doStep(field, new PutCommand(low_x, y, this)); error = "Stone successfully set"  //print(s"will put to ${low_x}\n")
-    case FullRow => error = (s"the vertical row at ${y+1} is full\n") //throw noAction
-    }    
+    } match {
+      case Success(x) =>
+      case Failure(e) =>
+        e match {
+          case SpaceFound =>
+            undoManager.clearRedo();
+            field = undoManager.doStep(field, new PutCommand(low_x, y, this));
+            error = "Stone successfully set"
+          case FullRow => error = (s"the vertical row at ${y + 1} is full\n")
+        }
+    }
     notifyObservers
-  
-  def undo = 
+
+  def undo =
     val oldfield = field
     field = undoManager.undoStep(field)
     if (field == oldfield)
@@ -52,8 +84,8 @@ case class Controller @Inject()(@Named("DefField") var field: FieldInterface) ex
     else
       error = "Undo erfolgreich"
     notifyObservers
-  
-  def redo = 
+
+  def redo =
     val oldfield = field
     field = undoManager.redoStep(field)
     if (field == oldfield)
@@ -61,12 +93,12 @@ case class Controller @Inject()(@Named("DefField") var field: FieldInterface) ex
     else
       error = "Redo erfolgreich"
     notifyObservers
-  
+
   def setMode(str: String): ModeStrategy = field.setMode(str)
-  def getPlayer: Stone = if(field.getPlayerState()) Stone.X else Stone.O
+  def getPlayer: Stone = if (field.getPlayerState()) Stone.X else Stone.O
   def setStrength(d: Int) = field.setDifficulty(d)
- 
-  def quit = 
+
+  def quit =
     killObservers
 
   def save: Unit =
--- src/main/scala/de/htwg/se/model/ComputerStrategyComponent/StrategyInterface.scala
+++ src/main/scala/de/htwg/se/model/ComputerStrategyComponent/StrategyInterface.scala
@@ -4,26 +4,49 @@ import scala.compiletime.ops.int
 import de.htwg.se.model.fieldComponent.FieldInterface
 import de.htwg.se.model.fieldComponent.fieldBaseImpl.Stone
 import java.lang.reflect.Field
+import scala.util.{Try, Success, Failure}
 
-trait Strategy { //concept uses Strategy Pattern but is in its own Folder
-    var globalY: Int = -1
-    //abstract var globalField : FieldInterface
-    var ComputerStone : Stone = Stone.O
+trait Strategy { // concept uses Strategy Pattern but is in its own Folder
+  var globalY: Int = -1
+  // abstract var globalField : FieldInterface
+  var ComputerStone: Stone = Stone.O
 
-    def put(field : FieldInterface) : (Int, Int)
+  def put(field: FieldInterface): (Int, Int)
 
-    def matchx(wantedX: Int, field: FieldInterface): Int = {
-        var low_x = 8 //provocate Crash if not handled
-        try {      
-            for (try_x <- field.size-1 to 0 by -1) { //gehe von unten los
-                if (field.get(try_x, globalY) == Stone.Empty) { low_x = try_x; throw SpaceFound; }
-            }
-            throw FullRow
-            } catch {
-            case SpaceFound => low_x
-            case FullRow => -1
-            }    
+//   def matchx(wantedX: Int, field: FieldInterface): Int = {
+//     var low_x = 8 // provocate Crash if not handled
+//     try {
+//       for (try_x <- field.size - 1 to 0 by -1) { // gehe von unten los
+//         if (field.get(try_x, globalY) == Stone.Empty) {
+//           low_x = try_x; throw SpaceFound;
+//         }
+//       }
+//       throw FullRow
+//     } catch {
+//       case SpaceFound => low_x
+//       case FullRow    => -1
+//     }
+//   }
+
+  def matchx(wantedX: Int, field: FieldInterface): Int =
+    var low_x = 8
+    val result = Try {
+      for (try_x <- field.size - 1 to 0 by -1) {
+        if (field.get(try_x, globalY) == Stone.Empty) {
+          low_x = try_x
+          throw SpaceFound
         }
+      }
+      throw FullRow
+    } match {
+      case Success(_) => low_x
+      case Failure(e) =>
+        e match {
+          case SpaceFound => low_x
+          case FullRow    => -1
+        }
+    }
+    result
 }
 
 //easy = random
