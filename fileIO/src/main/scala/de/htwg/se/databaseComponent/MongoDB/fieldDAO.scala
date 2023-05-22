c19944a20223f5319afdf57b3fb1ced8910e2e74
cr-jkb <81223644+cr-jkb@users.noreply.github.com>
2023-05-22 22:28:02 +0200

    Create MongoDB FieldDao
    in half pseudo code

--- fileIO/src/main/scala/de/htwg/se/databaseComponent/MongoDB/fieldDAO.scala
+++ fileIO/src/main/scala/de/htwg/se/databaseComponent/MongoDB/fieldDAO.scala
@@ -1,1 +1,59 @@
-object fieldDAO extends DAOInterface {}
+object fieldDAO extends DAOInterface {
+
+  val client: MongoClient = MongoClient()
+  val database: MongoDatabase = client.getDatabase("mydb")
+  val collection: MongoCollection[Document] =
+    database.getCollection("savegames")
+
+  def create(jsonField: String): Int = {
+    val document: Document = Document("_id" -> 1, "x" -> 1)
+    val query = collection.insertOne(jsonField)
+
+    query
+      .onComplete {
+        println("finish"); 0
+      }
+      .onError { (e) =>
+        println(s"$e"); 1
+      }
+  }
+
+  def read(id: Int): String = {
+    val document: Document = Document("_id" -> 1, "x" -> 1)
+    val query = collection.get(id)
+
+    query
+      .onComplete {
+        println("finish"); 0
+      }
+      .onError { (e) =>
+        println(s"$e"); 1
+      }
+  }
+
+  override def update(id: Int, jsonField: String): Unit = {
+    val document: Document = Document("_id" -> 1, "x" -> 1)
+    val query = collection.update(id, jsonField)
+
+    query
+      .onComplete {
+        println("finish"); 0
+      }
+      .onError { (e) =>
+        println(s"$e"); 1
+      }
+  }
+
+  override def delete(id: Int): Unit = {
+    val document: Document = Document("_id" -> 1, "x" -> 1)
+    val query = collection.delete(id)
+
+    query
+      .onComplete {
+        println("finish"); 0
+      }
+      .onError { (e) =>
+        println(s"$e"); 1
+      }
+  }
+}
