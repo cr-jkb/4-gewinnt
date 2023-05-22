a8b9e3e7ff028676063c928e06fde59ad0475925
yasmoonx <yasminhoffmann99@web.de>
2023-05-22 21:21:40 +0200

    add SlickImpl

--- fileIO/src/main/scala/de/htwg/se/databaseComponent/SlickImpl.scala
+++ fileIO/src/main/scala/de/htwg/se/databaseComponent/SlickImpl.scala
@@ -1,15 +1,18 @@
-/*
 import scala.io.Source
 import play.api.libs.json._
 import scala.concurrent.Await
 import scala.concurrent.duration._
-
+import de.htwg.se.databaseComponent.fieldDAO
 
 object SlickImpl {
-  
-    def loadGame():JsValue = {
-        Await.result(fieldDAO.read,15.seconds)
-    }
+
+  // read latest FieldTable entry
+  def load(): String = {
+    val id = 1;
+    fieldDAO.read(id)
+  }
+  def save(jsonField: String) = {
+    fieldDAO.create(jsonField)
+  }
 
 }
-*/
--- fileIO/src/main/scala/de/htwg/se/fileIOComponent/fileIOJsonImpl/fileIOJsonImpl.scala
+++ fileIO/src/main/scala/de/htwg/se/fileIOComponent/fileIOJsonImpl/fileIOJsonImpl.scala
@@ -7,17 +7,15 @@ import java.io.File
 import de.htwg.se.databaseComponent.fieldDAO
 
 object fileIOJsonImpl {
-  var latestID : Int = -1;
+  var latestID: Int = -1;
 
   def save(json: String): Unit = {
     val pw = PrintWriter(File("game.json"))
     pw.write(json)
-    latestID = fieldDAO.create(json);
     pw.close
 
   }
   def load(): String = {
-    fieldDAO.read(latestID)
     val source: String = Source.fromFile("game.json").getLines.mkString
     source
   }
