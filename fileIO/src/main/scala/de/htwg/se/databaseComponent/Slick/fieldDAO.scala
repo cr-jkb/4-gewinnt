1fc4c5e8b6e6b6c2042f7c9de4f3e7fa084767f7
yasmoonx <yasminhoffmann99@web.de>
2023-05-22 19:09:47 +0200

    fix type mismatch error

--- fileIO/src/main/scala/de/htwg/se/databaseComponent/Slick/fieldDAO.scala
+++ fileIO/src/main/scala/de/htwg/se/databaseComponent/Slick/fieldDAO.scala
@@ -16,7 +16,8 @@ object fieldDAO extends DAOInterface {
   val port = "5432"
   val dbname = "fileio"
   val db = Database.forURL(
-    url = "jdbc:postgresql://" + ip + ":" + port + "/" + dbname + "?serverTimezone=UTC",
+    url =
+      "jdbc:postgresql://" + ip + ":" + port + "/" + dbname + "?serverTimezone=UTC",
     user = sys.env.getOrElse("POSTGRES_USER", "username").toString,
     password = sys.env.getOrElse("POSTGRES_USER", "password").toString,
     driver = "org.postgresql.Driver"
@@ -40,23 +41,24 @@ object fieldDAO extends DAOInterface {
   }*/
 
   def create(jsonField: String): Int = {
-    val rand : Random = new Random();
+    val rand: Random = new Random();
     val id = rand.nextInt(900000);
     /*fieldTable += (id, jsonField);*/
     val query = DBIO.seq(
-      fieldTable.schema.createIfNotExists,
+      fieldTable.schema.createIfNotExists
     )
     val result = Future(Await.result(db.run(query), Duration.Inf))
     result.onComplete {
       case Success(obj) => println(s"Create $obj with $id"); return id;
-      case Failure(exc) => println(s"error on create: ${exc.getMessage}"); return -1;
+      case Failure(exc) =>
+        println(s"error on create: ${exc.getMessage}"); return -1;
     }
     return id;
   }
 
   override def read(id: Int): String = {
     val query = fieldTable.filter(_.id === id).result.headOption
-    val result = Future(Await.result(db.run(query), atMost=10.seconds))
+    val result = Await.result(db.run(query), atMost = 10.seconds)
     /*Json.toJson()*/
     result match {
       case Some(row) => row.toString
@@ -73,7 +75,8 @@ object fieldDAO extends DAOInterface {
   }
 
   override def delete(id: Int): Unit = {
-    val query = fieldTable.filter(_.id === id).result.headOption//.delete!?!?!?
+    val query =
+      fieldTable.filter(_.id === id).result.headOption // .delete!?!?!?
     val result = db.run(query)
 
     result.onComplete {
