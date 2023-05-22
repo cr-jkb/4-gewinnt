562e471bb4de3950d2c9f5f1c91ab634e2e07b8f
yasmoonx <yasminhoffmann99@web.de>
2023-05-22 19:06:01 +0200

    fix slick error

--- fileIO/src/main/scala/de/htwg/se/databaseComponent/Slick/FieldTable.scala
+++ fileIO/src/main/scala/de/htwg/se/databaseComponent/Slick/FieldTable.scala
@@ -3,7 +3,7 @@ package de.htwg.se.databaseComponent
 import slick.jdbc.PostgresProfile.api.*
 import slick.lifted.Tag
 
-class FieldTable(tag: Tag) extends Table[String](tag, "field") {
+class FieldTable(tag: Tag) extends Table[(Int, String)](tag, "field") {
 
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def jsonField = column[String]("jsonField")
