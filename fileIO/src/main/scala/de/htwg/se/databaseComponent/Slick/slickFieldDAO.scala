6b31f8c169d0ceb028715c98ed2d8012dc6e1d3f
yasmoonx <yasminhoffmann99@web.de>
2023-06-13 20:33:59 +0200

    ups

--- fileIO/src/main/scala/de/htwg/se/databaseComponent/Slick/slickFieldDAO.scala
+++ fileIO/src/main/scala/de/htwg/se/databaseComponent/Slick/slickFieldDAO.scala
@@ -14,6 +14,7 @@ import java.util.concurrent.locks.ReentrantLock
 
 object slickFieldDAO extends DAOInterface {
   var counter = 1;
+  private val counterLock = new ReentrantLock()
   val ip = "localhost"
   val port = "5432"
   val dbname = "postgres"
